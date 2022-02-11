f8f4a31b0403a91d8ca4e4ee1814638e974522e5
ZhandarbekCh <98944159+ZhandarbekCh@users.noreply.github.com>
2022-02-11 22:21:24 +0600

    Delete k.py

--- lab1/lab2/k.py
+++
@@ -1,9 +0,0 @@
-n =int(input())
-
-for i in range(len(n)):
-    for j in range(len(n[i])):
-
-        if n[i][j] in '?.,!-;:"' or n[i][j] in "'":
-            n[i] = n[i].replace(n[i][j], '')
-            
-print(len(set(n)), *sorted(set(n)), sep='\n')
