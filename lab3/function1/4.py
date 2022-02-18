cb551240b60541a16c342b6a0991e17e299ca980
ZhandarbekCh <98944159+ZhandarbekCh@users.noreply.github.com>
2022-02-18 21:50:14 +0600

    Create 4.py

---
+++ lab3/function1/4.py
@@ -0,0 +1,13 @@
+def filter_prime(x):
+    ans=[]
+    for i in range(len(x)):
+        count=True
+        for j in range(2,int(x[i])//2+1):
+            if(int(x[i])%j==0):
+                count=False
+                break
+        if(count):
+            ans.append(int(x[i]))
+    return ans
+x=input().split()
+print(*filter_prime(x))
